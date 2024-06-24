ce0982b9243ddf40a83a08f80ebea4761bb19e21
Diego Villanueva <diegovt.arbt@gmail.com>
2024-06-24 17:57:21 +0000

    test for delete of participations

--- tests/test_participations.py
+++ tests/test_participations.py
@@ -50,3 +50,51 @@ async def test_fetch_participation_by_id():
         assert response.status_code == 204
 
         await client.delete(API_URL + USER_SECTION + PHONE)
+
+
+@pytest.mark.asyncio
+async def test_delete_participation_by_id():
+    async with AsyncClient() as client:
+        user_phones = ["1234567890", "0987654321"]
+        users = [{"phone": phone, "terms": True}
+                 for phone in user_phones]
+
+        clients = []
+        for user in users:
+            response = await client.post(API_URL + USER_SECTION, json=user)
+            assert response.status_code == 201
+            cl = response.json()
+            clients.append(cl)
+
+        response = await client.get(FULL_URL)
+        assert response.status_code == 200
+
+        participations = response.json()
+        length = len(participations)
+
+        # create 3 participations for each user
+        for user in clients:
+            for i in range(3):
+                response = await client.post(FULL_URL, json={"user": user})
+                assert response.status_code == 201
+
+        response = await client.get(FULL_URL)
+        assert response.status_code == 200
+
+        participations = response.json()
+        assert len(participations) == length + 6
+
+        for participation in participations:
+            response = await client.delete(FULL_URL + participation["_id"])
+            assert response.status_code == 204
+
+        response = await client.get(FULL_URL)
+        assert response.status_code == 200
+
+        participations = response.json()
+        assert len(participations) == length
+
+        # delete users
+        for user in users:
+            response = await client.delete(API_URL + USER_SECTION + user["phone"])
+            assert response.status_code == 204
