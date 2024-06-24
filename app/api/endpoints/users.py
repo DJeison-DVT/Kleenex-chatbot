7849ef5c87be79847b6bd2d58e4dc94a4cda4523
Diego Villanueva <diegovt.arbt@gmail.com>
2024-06-24 02:31:25 +0000

    refactor convert to id str

--- app/api/endpoints/users.py
+++ app/api/endpoints/users.py
@@ -1,23 +1,24 @@
 from fastapi import APIRouter, HTTPException, Depends
 from app.schemas.user import User, UserCreation
-from pydantic import EmailStr
 from app.api.deps import get_db
-from app.serializers.user import serialize_user, serialize_users, serialize_user_dict
+from app.serializers.user import serialize_user, serialize_users
 
 router = APIRouter()
 
+
 @router.get("/")
-async def fetch_all_users(db = Depends(get_db), response_model=User):
+async def fetch_all_users(db=Depends(get_db), response_model=User):
     cursor = db.users.find({})
     users = []
     async for user in cursor:
-        users.append(serialize_user_dict(user))
+        users.append(user)
     return serialize_users(users)
+
 
 @router.post("/", response_model=User)
 async def create_user(
     user: UserCreation,
-    db = Depends(get_db)
+    db=Depends(get_db)
 ) -> User:
     phone = user.phone
     terms = user.terms
@@ -32,29 +33,32 @@ async def create_user(
         raise HTTPException(status_code=500, detail=f"Error: {e}")
     new_user = await db.users.find_one({"_id": result.inserted_id})
 
-    return serialize_user(serialize_user_dict(new_user))
+    return serialize_user(new_user)
+
 
 @router.delete("/{phone}")
-async def delete_user_by_phone(phone: str, db = Depends(get_db)):
+async def delete_user_by_phone(phone: str, db=Depends(get_db)):
     user = await db.users.find_one({"phone": phone})
     if not user:
         raise HTTPException(status_code=404, detail="User not found")
     await db.users.delete_one({"phone": phone})
     return {"message": "User deleted successfully"}
 
+
 @router.get("/{phone}")
-async def fetch_user_by_phone(phone: str, db = Depends(get_db), response_model=User):
+async def fetch_user_by_phone(phone: str, db=Depends(get_db), response_model=User):
     user = await db.users.find_one({"phone": phone})
     if not user:
         raise HTTPException(status_code=404, detail="User not found")
-    return serialize_user(serialize_user_dict(user))
+    return serialize_user(user)
+
 
 @router.put("/{phone}")
-async def update_user_by_phone(phone: str, user: User, db = Depends(get_db), response_model=User):
+async def update_user_by_phone(phone: str, user: User, db=Depends(get_db), response_model=User):
     old_user = await db.users.find_one({"phone": phone})
     if not old_user:
         raise HTTPException(status_code=404, detail="User not found")
-                    
+
     id = old_user.get('_id')
     try:
         await db.users.update_one({"_id": id}, {"$set": {
@@ -67,6 +71,6 @@ async def update_user_by_phone(phone: str, user: User, db = Depends(get_db), res
     except Exception as e:
         print(e)
         raise HTTPException(status_code=500, detail=f"Error: {e}")
-    
+
     user = await db.users.find_one({"_id": id})
-    return serialize_user(serialize_user_dict(user))
+    return serialize_user(user)
--- app/serializers/user.py
+++ app/serializers/user.py
@@ -2,19 +2,19 @@ from typing import List
 from functools import wraps
 from app.schemas import User
 
+
 def convert_id_to_str(func):
     @wraps(func)
     def wrapper(user_dict):
         # Convert '_id' to string if it exists
         if '_id' in user_dict:
             user_dict['_id'] = str(user_dict['_id'])
-        return func(user_dict)
+            user = User(**user_dict)
+        return func(user)
     return wrapper
 
-@convert_id_to_str
-def serialize_user_dict(user_dict: dict):
-    return User(**user_dict)
 
+@convert_id_to_str
 def serialize_user(user: User):
     return {
         "_id": str(user.id),
@@ -22,8 +22,10 @@ def serialize_user(user: User):
         "terms": user.terms,
         "name": user.name if user.name else None,
         "email": user.email if user.email else None,
-        "flow_step": user.flow_step
+        "flow_step": user.flow_step,
+        # "participations": user.participations
     }
+
 
 def serialize_users(users: List[User]):
     return [serialize_user(user) for user in users]
