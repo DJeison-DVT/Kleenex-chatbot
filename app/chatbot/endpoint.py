39d806bec5a598d8067332a186e58bbde11ff104
Diego Villanueva <diegovt.arbt@gmail.com>
2024-06-25 19:59:39 +0000

    handle unkwnow phone and messages

--- app/chatbot/endpoint.py
+++ app/chatbot/endpoint.py
@@ -3,9 +3,10 @@ import urllib.parse
 import requests
 from twilio.rest import Client
 from httpx import AsyncClient
-from fastapi import APIRouter, Request
+from fastapi import APIRouter, Request, HTTPException
 
 from app.core.config import settings
+from app.chatbot.messages_offline import messages
 
 client = Client(settings.TWILIO_ACCOUNT_SID, settings.TWILIO_AUTH_TOKEN)
 
@@ -60,16 +61,85 @@ class Message:
 
 
 def send_message(message: Message, body: str):
-    client.messages.create(
-        from_=message.to_number,
-        body=body,
-        to=message.from_number,
-    )
+    try:
+        client.messages.create(
+            from_=message.to_number,
+            body=body,
+            to=message.from_number,
+        )
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error: {e}")
 
 
-@router.post("/")
+async def get_current_ticket_number(client: AsyncClient):
+    try:
+        response = await client.get(
+            f"{settings.BASE_URL}{settings.API_STR}{settings.PARTICIPATION_SECTION}/count")
+        response.raise_for_status()
+    except Exception as e:
+        raise HTTPException(
+            status_code=500, detail=f"Failed to get current ticket number: {str(e)}")
+
+    count = response.json()
+    return count['count']
+
+
+async def get_user(client: AsyncClient, message):
+    print("getting user...")
+    endpoint = f"{settings.BASE_URL}{settings.API_STR}{settings.USER_SECTION}/{message.from_number}"
+    response = await client.get(endpoint)
+    if response.status_code == 404:
+        return None
+
+    if response.status_code != 200:
+        raise HTTPException(status_code=500, detail="Failed to fetch user")
+
+    user = response.json()
+    return user
+
+
+async def handle_user(client: AsyncClient, user, message):
+    print("handling user...")
+
+    match user.flow:
+        case "onboarding":
+            pass
+        case _:
+            pass
+
+
+async def handle_new_user(client: AsyncClient, message):
+    print(f"New user: {message.from_number}")
+    response = await client.post(f"{settings.BASE_URL}{settings.API_STR}{settings.USER_SECTION}/",
+                                 json={"phone": message.from_number})
+
+    if response.status_code != 201:
+        raise HTTPException(status_code=500, detail="Failed to create user")
+
+    count = await get_current_ticket_number(client)
+    send_message(message, messages["welcome"].format(tickets_registered=count))
+
+
+@ router.post("/")
 async def webhook(request: Request):
-    body_bytes = await request.body()
-    message = Message(body_bytes)
+    message = None
+    try:
+        body_bytes = await request.body()
+        message = Message(body_bytes)
 
-    return {"message": "Received", "from": message.from_number}
+        print(
+            f"Received message: {message.body_content}, from: {message.from_number}")
+
+        async with AsyncClient() as client:
+            user = await get_user(client, message)
+            if user:
+                await handle_user(client, user, message)
+            else:
+                await handle_new_user(client, message)
+
+        return {"message": "Received", "from": message.from_number}
+    except HTTPException as http_exc:
+        send_message(message, messages["error"])
+        return {"error code": http_exc.status_code, "error": http_exc.detail}
+    except Exception as exc:
+        return {"error": f"An unexpected error occurred: {str(exc)}"}
