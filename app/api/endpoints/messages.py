ec3e91615b0c9176e6415adbea830be842fcab3a
DJeison <diegovt.arbt@gmail.com>
2024-07-31 16:04:04 -0600

    make messages endpoints authenticated

--- app/api/endpoints/messages.py
+++ app/api/endpoints/messages.py
@@ -1,12 +1,18 @@
-from fastapi import APIRouter, HTTPException, Response, Query
+from fastapi import APIRouter, HTTPException, Response, Depends, Query
+from typing import Annotated
 
 from app.chatbot.messages import get_user_messages
+from app.core.auth import RoleChecker
 
 router = APIRouter()
 
 
 @router.get("/")
-async def fetch_user_messages(response: Response, id: str = Query(..., description="The phone number of the user")):
+async def fetch_user_messages(
+    response: Response,
+    _: Annotated[bool, Depends(RoleChecker(allowed_roles=["admin"]))],
+    id: str = Query(..., description="The phone number of the user")
+):
     messages = await get_user_messages(id)
     if not messages:
         raise HTTPException(
